c7a4fc1c1fbcacbc60fdcf4983a3e653521367e7
Leonardo Martinez Davydov <40177373@hcuc.ac.uk>
2022-05-09 14:50:01 +0100

    stinker

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.fart
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
+          
           target="_blank"
           rel="noopener noreferrer"
         >
